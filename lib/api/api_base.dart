cd80e904abfbb54af73ab822904888daadf79fff
GideonMax <45212668+GideonMax@users.noreply.github.com>
2021-01-19 18:54:39 +0200

    made small changes to user api and consolidated basic api utilities into one file

---
+++ lib/api/api_base.dart
@@ -0,0 +1,50 @@
+export 'dart:convert';
+export 'dart:typed_data';
+export 'package:gatecheck_frontend/utils/string_uuid_conversion.dart';
+
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+enum DataStatus { Success, Unauthorized, Failure }
+
+String api = '/api/v1';
+class ApiData<T> {
+  DataStatus status;
+  T data;
+
+  ApiData(T Data, DataStatus Status)
+      : data = Data,
+        status = Status;
+}
+
+
+///this function takes a response and a toJson constructor for a certain type and returns an ApiData object for the specified type.
+ApiData<T> parseResponse<T>(http.Response res, T toJson(dynamic json)){
+  if(res.statusCode==200){
+    var json=jsonDecode(res.body);
+    return ApiData(toJson(json), DataStatus.Success);
+  }
+
+  if (res.statusCode == 401) {
+    return ApiData<T>(null, DataStatus.Unauthorized);
+  }
+  return ApiData<T>(null, DataStatus.Failure);
+}
+
+
+///this function takes a response with no body and returns an ApiData<void> according to the status code.
+ApiData<void> emptyResponseData(http.Response res) {
+  DataStatus status;
+  switch (res.statusCode) {
+    case 200:
+      status = DataStatus.Success;
+      break;
+    case 401:
+      status = DataStatus.Unauthorized;
+      break;
+    default:
+      status = DataStatus.Failure;
+      break;
+  }
+  return ApiData(null, status);
+}
--- lib/api/user_api.dart
+++ lib/api/user_api.dart
@@ -1,32 +1,23 @@
 import 'package:http/http.dart' as http;
 import 'package:gatecheck_frontend/model/user_model.dart';
-import 'dart:convert';
-import 'package:gatecheck_frontend/api/api_data.dart';
+import 'package:gatecheck_frontend/api/api_base.dart';
 
-String api = '/api/v1';
 
 Future<ApiData<GenericUser>> currentUser(http.Client client) {
   return client.get(api + '/user').then((res) {
-    if (res.statusCode == 200) {
-      var json = jsonDecode(res.body);
-      return ApiData(GenericUser.fromJson(json), DataStatus.Success);
-    }
-    if (res.statusCode == 401) {
-      return ApiData<GenericUser>(null, DataStatus.Unauthorized);
-    }
-    return ApiData<GenericUser>(null, DataStatus.Failure);
+    return parseResponse(res, (json)=>GenericUser.fromJson(json));
   });
 }
 
 Future<ApiData<List<Student>>> getStudents(
-    List<int> studentIds, bool all, http.Client client) {
+    Int32x4List studentIds, bool all, http.Client client) {
   String query;
   if (all) {
     query = "all=true";
   } else {
-    query = "uuid=" + studentIds.join() + "&all=false";
+    query = "uuid=" + studentIds.map(uuidToString).join(',') + "&all=false";
   }
-  
+
   return client.get(api + '/user?' + query).then((res) {
     if (res.statusCode == 200) {
       List<Map<String, dynamic>> json =
@@ -53,20 +44,4 @@ Future<ApiData<void>> updateSpecificUser(GenericUser user, http.Client client) {
   return client
       .put(api + '/user/' + data['id'], body: data)
       .then(emptyResponseData);
-}
-
-ApiData<void> emptyResponseData(http.Response res) {
-  DataStatus status;
-  switch (res.statusCode) {
-    case 200:
-      status = DataStatus.Success;
-      break;
-    case 401:
-      status = DataStatus.Unauthorized;
-      break;
-    default:
-      status = DataStatus.Failure;
-      break;
-  }
-  return ApiData(null, status);
 }
